0c76aea0a6bf9c0cd8aee44b93f3fa52472fd4d8
CoreyBabs <coreymbabcock@gmail.com>
2023-05-27 10:13:03 -0600

    remove unneeded comments from template

--- lib/main.dart
+++ lib/main.dart
@@ -47,10 +47,7 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-     // _counter++;
+      // so that the display can reflect the updated values.
     });
   }
 
@@ -66,34 +63,13 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.primary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title,
         style: TextStyle(color: Theme.of(context).colorScheme.inversePrimary)),
       ),
       body: 
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.start,
-         // mainAxisSize: MainAxisSize.min,
           children: <Widget>[
             TableCalendar (
               firstDay: firstDay,
@@ -134,25 +110,6 @@ class _MyHomePageState extends State<MyHomePage> {
               focusedDay = newFocusedDay;
             },), 
             const Padding(padding: EdgeInsets.symmetric(vertical: 10)),
-            // Row(
-            //   children: [
-            //     Text(
-            //       'Tags:',
-            //       style: Theme.of(context).textTheme.headlineSmall
-            //     ),
-            //     const Padding(
-            //       padding: EdgeInsets.symmetric(horizontal: 7)),
-            //     for (var tag in tags)
-            //       Container(
-            //         decoration: BoxDecoration(
-            //           shape: BoxShape.rectangle,
-            //           color: tag.color,
-            //           borderRadius: BorderRadius.circular(2.0),
-            //         ),
-            //         margin: const EdgeInsets.symmetric(horizontal: 3),
-            //         alignment: Alignment.center,
-            //         child: Text(tag.name),),
-            // ],)
             Align(
               alignment: Alignment.centerLeft,
               child: Text(
@@ -198,7 +155,6 @@ class _MyHomePageState extends State<MyHomePage> {
             const Padding(
               padding: EdgeInsets.symmetric(vertical: 9)),         
             Flexible(
-              // constraints: const BoxConstraints(maxWidth: 50),
               child:
                 ListView(
                   shrinkWrap: true,
